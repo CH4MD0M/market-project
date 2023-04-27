958246d8e48ece423104b92fd25627111cb10316
KiHoon Roh <started677@gmail.com>
2023-04-27 15:11:30 +0900

    [feat] pagination 구현

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,5 +1,22 @@
-const getProducts = (req, res) => {
-  res.send('Handling product routes, e.g. search for products');
-};
+const Product = require('../models/productModel');
+const recordsPerPage = require('../config/pagination');
 
+const getProducts = async (req, res, next) => {
+  try {
+    const pageNum = Number(req.query.pageNum) || 1;
+    const totalProducts = await Product.countDocuments({});
+    const products = await Product.find({})
+      .skip(recordsPerPage * (pageNum - 1))
+      .sort({ name: 1 })
+      .limit(recordsPerPage);
+
+    res.json({
+      products,
+      pageNum,
+      paginationLinksNumber: Math.ceil(totalProducts / recordsPerPage),
+    });
+  } catch (error) {
+    next(error);
+  }
+};
 module.exports = getProducts;
