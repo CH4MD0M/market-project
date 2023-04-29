77b6ccf58023f507508d32a0290233a9b4e5c9f4
KiHoon Roh <started677@gmail.com>
2023-04-29 19:34:15 +0900

    [chore] backend scripts 수정

--- backend/server.js
+++ backend/server.js
@@ -22,14 +22,20 @@ app.use('/api', apiRoutes);
 
 // Error handling
 app.use((error, req, res, next) => {
-  console.error(error);
+  if (process.env.NODE_ENV === 'development') console.error(error);
   next(error);
 });
 app.use((error, req, res, next) => {
-  res.status(500).json({
-    message: error.message,
-    stack: error.stack,
-  });
+  if (process.env.NODE_ENV === 'development') {
+    res.status(500).json({
+      message: error.message,
+      stack: error.stack,
+    });
+  } else {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
