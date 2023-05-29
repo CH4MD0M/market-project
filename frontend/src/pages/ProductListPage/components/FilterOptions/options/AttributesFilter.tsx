ca4cb14129dbb48e2821599df782b0d94a8a5c19
KiHoon Roh <started677@gmail.com>
2023-05-29 15:15:12 +0900

    [feat] 필터 기능 구현

--- frontend/src/pages/ProductListPage/components/FilterOptions/options/AttributesFilter.tsx
+++ frontend/src/pages/ProductListPage/components/FilterOptions/options/AttributesFilter.tsx
@@ -1,21 +1,81 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Form } from 'react-bootstrap';
 
+import { useAppDispatch, useAppSelector } from '@hooks/reduxHooks';
+import { setAttrsFilter, setAttrsFromFilter } from '@redux/modules/filterSlice';
+
 const AttributesFilter = () => {
+  const dispatch = useAppDispatch();
+  const { categories } = useAppSelector(state => state.category);
+  const { attrsFilter, attrsFromFilter, categoriesFromFilter } = useAppSelector(
+    state => state.filter,
+  );
+
+  const attrOnchangeHandler = (
+    e: React.ChangeEvent<HTMLInputElement>,
+    selectedAttr: AttrsFilter,
+    valueForSelectedAttr: string,
+  ) => {
+    const newAttrsFilter = modifiedAttrsFilter(
+      e,
+      selectedAttr,
+      valueForSelectedAttr,
+      attrsFromFilter,
+    );
+    dispatch(setAttrsFromFilter(newAttrsFilter));
+  };
+
+  const modifiedAttrsFilter = (
+    e: React.ChangeEvent<HTMLInputElement>,
+    selectedAttr: AttrsFilter,
+    valueForSelectedAttr: string,
+    attrsFromFilter: AttrsFilter[],
+  ) => {
+    const filterIndex = attrsFromFilter.findIndex(item => item.key === selectedAttr.key);
+
+    if (filterIndex === -1) {
+      return [...attrsFromFilter, { key: selectedAttr.key, value: [valueForSelectedAttr] }];
+    }
+
+    const updatedFilters = [...attrsFromFilter];
+    const filterValues = new Set(updatedFilters[filterIndex].value);
+
+    if (e.target.checked) filterValues.add(valueForSelectedAttr);
+    else filterValues.delete(valueForSelectedAttr);
+
+    const updatedValues = Array.from(filterValues);
+
+    updatedFilters[filterIndex] =
+      updatedValues.length > 0 ? { ...updatedFilters[filterIndex], value: updatedValues } : null;
+
+    return updatedFilters.filter(Boolean);
+  };
+
+  useEffect(() => {
+    dispatch(setAttrsFilter([]));
+    Object.entries(categoriesFromFilter).forEach(([category, checked]) => {
+      if (checked) {
+        const UpdatedAttrs = categories.find(item => item.name === category).attrs;
+        dispatch(setAttrsFilter(UpdatedAttrs));
+      }
+    });
+  }, [categoriesFromFilter, categories]);
+
   return (
     <>
-      {[
-        { 색상: ['red', 'green', 'blue'] },
-        {
-          사이즈: ['S', 'M', 'L', 'XL', 'XXL'],
-        },
-      ].map((item, idx) => (
-        <div key={idx} className="mb-3">
+      {attrsFilter?.map((attr, idx) => (
+        <div key={idx}>
           <Form.Label>
-            <b>{Object.keys(item)}</b>
+            <b>{attr.key}</b>
           </Form.Label>
-          {Object.values(item)[0].map((value, idx) => (
-            <Form.Check key={idx} type="checkbox" id="default-checkbox" label={value} />
+          {attr?.value.map((value, value_idx) => (
+            <Form.Check
+              className="mb-1"
+              type="checkbox"
+              key={value_idx}
+              label={value}
+              onChange={e => attrOnchangeHandler(e, attr, value)}
+            />
           ))}
         </div>
       ))}
--- frontend/src/pages/ProductListPage/components/FilterOptions/options/RatingFilter.tsx
+++ frontend/src/pages/ProductListPage/components/FilterOptions/options/RatingFilter.tsx
@@ -2,14 +2,28 @@ import React, { Fragment } from 'react';
 import { Rating } from 'react-simple-star-rating';
 import { Form } from 'react-bootstrap';
 
+import { useAppDispatch, useAppSelector } from '@hooks/reduxHooks';
+import { setRatingsFromFilter } from '@/redux/modules/filterSlice';
+
 const RatingFilter = () => {
+  const dispatch = useAppDispatch();
+  const { ratingsFromFilter } = useAppSelector(state => state.filter);
+
+  const ratingOnChangeHandler = (e: React.ChangeEvent<HTMLInputElement>, idx: number) => {
+    dispatch(setRatingsFromFilter({ ...ratingsFromFilter, [5 - idx]: e.target.checked }));
+  };
+
   return (
     <>
       <span className="fw-bold">별점</span>
       {Array.from({ length: 5 }).map((_, idx) => (
         <Fragment key={idx}>
           <Form.Check type="checkbox" id={`check-api-${idx}`}>
-            <Form.Check.Input type="checkbox" isValid />
+            <Form.Check.Input
+              type="checkbox"
+              isValid
+              onChange={e => ratingOnChangeHandler(e, idx)}
+            />
             <Form.Check.Label style={{ cursor: 'pointer' }}>
               <Rating readonly size={20} initialValue={5 - idx} />
             </Form.Check.Label>
