7f8340cc06357f695ef1a7642182d3f5a69ea030
KiHoon Roh <started677@gmail.com>
2023-07-12 21:18:47 +0900

    [chore] css loader, splitChunks 설정

--- frontend/webpack.prod.js
+++ frontend/webpack.prod.js
@@ -1,13 +1,30 @@
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
 
-const TerserPlugin = require('terser-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-
 module.exports = merge(common, {
   mode: 'production',
-  devtool: 'cheap-module-source-map',
-  plugins: [new CleanWebpackPlugin()],
+  devtool: 'source-map',
+  module: {
+    rules: [
+      {
+        test: /\.(sa|sc|c)ss$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'css/[name].[contenthash:8].css',
+      chunkFilename: 'css/[id].[contenthash:8].css',
+    }),
+
+    new BundleAnalyzerPlugin(),
+  ],
   optimization: {
     usedExports: true,
     minimize: true,
@@ -19,9 +36,25 @@ module.exports = merge(common, {
           },
         },
       }),
+      new CssMinimizerPlugin({
+        minimizerOptions: {
+          preset: [
+            'default',
+            {
+              discardComments: { removeAll: true },
+            },
+          ],
+        },
+      }),
     ],
     splitChunks: {
-      chunks: 'all',
+      cacheGroups: {
+        vendor: {
+          test: /[\\/]node_modules[\\/]/,
+          name: 'vendors',
+          chunks: 'all',
+        },
+      },
     },
   },
 });
