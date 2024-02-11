ff6b81d8ac55e0691a305ca382835da640ebd94f
KiHoon Roh <started677@gmail.com>
2024-02-11 14:42:35 +0900

    [delete] socket 관련 기능 미구현으로 삭제

--- frontend/src/utils/api/analyticsApi.ts
+++
@@ -1,8 +0,0 @@
-import { API_URL } from '../constants';
-import { instance } from './instance';
-
-export const getAnalyticsDataByDate = async (date: string, signal: AbortSignal) => {
-  const { data } = await instance.get(API_URL.ANALYTICS.GET_ORDER_DATA(date), { signal });
-
-  return data;
-};
