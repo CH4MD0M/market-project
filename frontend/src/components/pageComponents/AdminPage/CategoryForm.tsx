eae4824efe63542d9116c6a8a9518673c7e15e86
KiHoon Roh <started677@gmail.com>
2024-03-30 16:21:41 +0900

    [update] 관리자 상품 생성 페이지 tailwind 적용

--- frontend/src/components/pageComponents/AdminPage/CategoryForm.tsx
+++ frontend/src/components/pageComponents/AdminPage/CategoryForm.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from 'react';
+import { XMarkIcon } from '@heroicons/react/24/outline';
 
 import { useAppDispatch, useAppSelector } from '@hooks/reduxHooks';
 import { setCategoryAttributes } from '@redux/modules/productSlice';
@@ -44,25 +45,15 @@ const CategoryForm = () => {
   }, [selectedCategory, dispatch, categoryDataList]);
 
   return (
-    <>
-      {/* 카테고리 선택 */}
-      {/* <Form.Group className="mb-3" controlId="formBasicCategory">
-        <Form.Label>
-          카테고리
-          {!isEditMode && (
-            <>
-              <CloseButton onClick={deleteCategoryHandler} />(<small>선택 카테고리 삭제</small>)
-            </>
-          )}
-        </Form.Label>
-        <Form.Select
+    <div className="mt-[30px] p-3 border border-gray-300 rounded-md">
+      <span className="block min-w-[120px] font-semibold text-[20px]">카테고리</span>
+      <div className="flex items-center">
+        <select
           id="cats"
-          required
-          name="category"
-          aria-label="Default select example"
           disabled={isEditMode}
           value={selectedCategory}
           onChange={categoryOnchangeHandler}
+          className="h-8 w-[300px] border border-gray-300 rounded-md px-2 mr-[15px]"
         >
           <option value="">카테고리 선택</option>
           {categoryDataList?.map((category, idx) => {
@@ -76,23 +67,33 @@ const CategoryForm = () => {
               </option>
             );
           })}
-        </Form.Select>
-      </Form.Group> */}
+        </select>
+        {!isEditMode && (
+          <div className="flex items-center group cursor-pointer">
+            <XMarkIcon
+              className="w-5 h-5 duration-150 group-hover:text-[red]"
+              onClick={deleteCategoryHandler}
+            />
+            <span className="duration-150 group-hover:text-[red]">(선택 카테고리 삭제)</span>
+          </div>
+        )}
+      </div>
 
       {/* 새 카테고리 추가 */}
-      {/* {!isEditMode && (
-        <Form.Group className="mb-3" controlId="formBasicNewCategory">
-          <Form.Label>새 카테고리 추가</Form.Label>
-          <Form.Control
+      {!isEditMode && (
+        <div className="mt-[20px]">
+          <span className="block min-w-[160px] font-semibold text-[18px]">새 카테고리 추가</span>
+          <input
+            className="px-2 border border-gray-300 rounded-md h-8 w-[300px]"
             onKeyUp={addNewCategoryHandler}
             onChange={e => setNewCategoryInput(e.currentTarget.value)}
             value={newCategoryInput}
             name="newCategory"
             type="text"
           />
-        </Form.Group>
-      )} */}
-    </>
+        </div>
+      )}
+    </div>
   );
 };
 
